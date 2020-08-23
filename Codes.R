389a5d517eff6af52c458b35049c73be01a3c3d2
Darwin22 <31376160+Darwin22@users.noreply.github.com>
2020-08-23 13:12:39 -0400

    Data and codes for the causes of death project

---
+++ Codes.R
@@ -0,0 +1,8 @@
+#####------------ Project------------------------------#####
+
+# download libraries needed
+library(tidyverse)
+library(plotly)
+library(gapminder)
+library(gganimate)
+library(gridExtra) 
